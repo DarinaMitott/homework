6eaedb4dba2dfefea22638e38c9f05e13c4a90d8
Darina <darinahohlova06@gmail.com>
2021-08-18 15:00:36 +0300

    dz2(leap year) way with if,else. dz2(coins) has been done.

--- coins/coins.py
+++ coins/coins.py
@@ -4,13 +4,23 @@
 # Найдите максимальную сумму из любых двух монет.
 # Номинальная стоимость монет: a, b и с тугриков.
 
+
 def max_sum_of_2(a: int, b: int, c: int) -> int:
-    pass
+    coins = [a, b, c]
+    coins.sort(reverse=True)
+    return coins[0] + coins[1]
+
+
+def prompt_and_check(name) -> int:
+    value = int(input(name + " = "))
+    if value <= 0:
+        raise Exception(f'u are invalid, ur variable {name} <= 0')
+    return value
 
 
 if __name__ == "__main__":
-    a = int(input("a = "))
-    b = int(input("a = "))
-    c = int(input("a = "))
+    a = prompt_and_check('a')
+    b = prompt_and_check('b')
+    c = prompt_and_check('c')
 
     print(max_sum_of_2(a, b, c))
--- leap_year/leap_year.py
+++ leap_year/leap_year.py
@@ -19,7 +19,14 @@
 
 
 def is_leap_year(year: int) -> bool:
-    return (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)
+    if year % 400 == 0:
+        return True
+    elif year % 100 != 0 and year % 4 == 0:
+        return True
+    return False
+
+    # return (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)
+
 
 if __name__ == "__main__":
     year = int(input("Put a year: "))
