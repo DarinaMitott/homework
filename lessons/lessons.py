862435cced196457ed37f91fda466e9dd45507b4
Darina <darinahohlova06@gmail.com>
2021-08-23 18:23:51 +0300

    lessons exercise has been done

--- lessons/lessons.py
+++ lessons/lessons.py
@@ -11,7 +11,14 @@
 
 
 def end_of_lesson(n: int) -> (int, int):
-    return 0, 0
+    if n <= 0:
+        raise Exception('u entered incorrect number of lesson')
+    if n == 1:
+        return 8, 45
+    spent = n * 45 + (n // 2) * 5 + ((n - 1) // 2) * 15
+    h = 8 + spent // 60
+    m = spent % 60
+    return h, m
 
 
 if __name__ == "__main__":
