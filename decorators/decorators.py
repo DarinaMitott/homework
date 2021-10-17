ccde27f55e1c5b59eb92f60fb47c33f76c608120
Darina <darinahohlova06@gmail.com>
2021-10-17 17:13:15 +0300

    dz 6: decorators

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -13,11 +13,33 @@ continue_on_errors со значением по умолчанию False.
 
 В случае любого несоответствия необходимо сгенерировать исключение TypeError.
 """
+from functools import wraps
 
 
-def type_check():
-    pass
+def type_check(*types):
+    def _type_check(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            if len(types) != len(args):
+                raise TypeError('invalid number of arguments')
+            for i in range(len(args)):
+                if not isinstance(args[i], types[i]):
+                    raise TypeError(f'Positional arg #{i} has type "{type(args[i]).__name__}" but must be {types[i].__name__}')
+            return f(*args, **kwargs)
+        return inner
+    return _type_check
 
 
-def repeat():
-    pass
+def repeat(n=2, continue_on_errors=False):
+    def _repeat(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            for _ in range(n):
+                try:
+                    f(*args, **kwargs)
+                except Exception as error:
+                    print(f'error: {repr(error)}')
+                    if not continue_on_errors:
+                        break
+        return inner
+    return _repeat
